f6986d1512b25f569e718ec150a7311cd1cdbf12
bitranox <rnowotny1966@gmail.com>
2025-10-02 22:27:44 +0200

    codespace

--- packaging/nix/flake.nix
+++ packaging/nix/flake.nix
@@ -77,7 +77,7 @@
       {
         packages.default = pypkgs.buildPythonPackage {
           pname = "lib_log_rich";
-          version = "11.1.1";
+          version = "12.1.1";
           pyproject = true;
           src = ../..;
           nativeBuildInputs = [ hatchlingVendor ];
